c5658d5e6bb4993b27aaa2b1cb4119f28136f2e4
n3ling <nling@myseneca.ca>
2024-11-14 17:57:13 -0500

    add type validation to create/patch using class-validation and class-transformer

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsEnum(["INTERN", "ADMIN", "ENGINEER"], {message: 'Valid role required'})
+    role: "INTERN" | "ADMIN" | "ENGINEER";
+}
