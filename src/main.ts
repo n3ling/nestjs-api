c031a3eba80c168dac45d91941408d2b2ad9094c
n3ling <nling@myseneca.ca>
2024-11-24 19:51:32 -0500

    add all exceptions filter for logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
+//import { MyLoggerModule } from './my-logger/my-logger.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+ 
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
+
+  //app.useLogger(app.get(MyLoggerModule))
   app.enableCors()
   app.setGlobalPrefix('api')
   await app.listen(3000);
